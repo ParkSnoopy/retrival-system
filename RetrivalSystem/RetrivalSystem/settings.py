e801250f5096dab6e2214250f3d176b541c66973
ParkSnoopy <sunwoo2539@gmail.com>
2023-06-03 20:23:06 +0800

    Update settings.py

--- RetrivalSystem/RetrivalSystem/settings.py
+++ RetrivalSystem/RetrivalSystem/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 CUSTOM_DOMAINS = os.environ["DOMAINS"].split("|||")
+
 ALLOWED_HOSTS = [
     *CUSTOM_DOMAINS
 ]
