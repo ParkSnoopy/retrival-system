8c8129afd9a370de44a33210d18da43e12dc5306
ParkSnoopy <sunwoo2539@gmail.com>
2023-06-10 18:30:09 +0800

    added django-import-export

--- RetrivalSystem/database/admin.py
+++ RetrivalSystem/database/admin.py
@@ -1,15 +1,41 @@
 from django.contrib import admin
 
 from .models import (
+    Article, 
+
     Organization, 
     Category, 
-    Article, 
     Region, 
+    
+    Tag, 
+    PrimaryTags, 
+    SecondaryTags, 
 )
+
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
-admin.site.register(Organization)
-admin.site.register(Category)
-admin.site.register(Region)
-admin.site.register(Article)
+
+class ArticleAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(Article, ArticleAdmin)
+
+
+class OrganizationAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(Organization, OrganizationAdmin)
+
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(Category, CategoryAdmin)
+
+class RegionAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(Region, RegionAdmin)
+
+
+class TagAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(Tag, TagAdmin)
+
+class PrimaryTagsAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(PrimaryTags, PrimaryTagsAdmin)
+
+class SecondaryTagsAdmin(ImportExportModelAdmin, admin.ModelAdmin):...
+admin.site.register(SecondaryTags, SecondaryTagsAdmin)
