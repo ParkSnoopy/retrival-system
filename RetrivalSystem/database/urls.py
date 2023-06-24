7091365c6f12194e8d7ef66f441fadf05aa07876
ParkSnoopy <117149837+ParkSnoopy@users.noreply.github.com>
2023-06-24 17:01:01 +0800

    delete database/data for possible 'import-export'

--- RetrivalSystem/database/urls.py
+++ RetrivalSystem/database/urls.py
@@ -5,11 +5,11 @@ Created on Sun Jun  4 19:25:18 2023
 @author: admin
 """
 
-from django.urls import path
-from .views import (
-    create, 
-)
+# from django.urls import path
+# from .views import (
+#     create, 
+# )
 
 urlpatterns = [
-    path('create/', create, name='database-create'), 
+    # path('create/', create, name='database-create'), 
 ]
--- RetrivalSystem/database/views.py
+++ RetrivalSystem/database/views.py
@@ -1,25 +1,25 @@
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import user_passes_test
+# from django.shortcuts import redirect
+# from django.contrib.auth.decorators import user_passes_test
 
-from .db_control import read_and_create_from
+# from .db_control import read_and_create_from
 
-import traceback
+# import traceback
 
 # Create your views here.
 
 
-@user_passes_test( lambda user: user.is_superuser )
-def create(request):
+# @user_passes_test( lambda user: user.is_superuser )
+# def create(request):
     
-    response = redirect('homepage-home', status='0')
+#     response = redirect('homepage-home', status='0')
     
-    try:
-        read_and_create_from("data.csv")
-        response.set_cookie('db_create', '0')
-    except Exception as exc:
-        print(f"\n\n  While building DB : {exc}\n")
-        traceback.print_exc()
-        print('\n')
-        response.set_cookie('db_create', '1')
+#     try:
+#         read_and_create_from("data.csv")
+#         response.set_cookie('db_create', '0')
+#     except Exception as exc:
+#         print(f"\n\n  While building DB : {exc}\n")
+#         traceback.print_exc()
+#         print('\n')
+#         response.set_cookie('db_create', '1')
     
-    return response
+#     return response
